c328d199c200b1e012d07714c846b6a171fdaeb4
Zayar MoeKaung <zayarmoekaung0@gmail.com>
2023-03-24 16:24:25 +0630

    started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,40 @@
+"use client";
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Link from 'next/link'
-const inter = Inter({ subsets: ['latin'] })
+import SA from 'scroll-animations-js';
+import  'scroll-animations-js/dist/css/index.min.css';
+
 
 export default function Home() {
+  useEffect(() => SA.init(), []);
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+     <div className={styles.hero}>
+     <p className={`${styles.frame} sa-animation sa-fade-right`} >
+      
+     <Image 
+     src='/img/zayar.png'
+     width={300}
+     height={300}
+     alt='zayar'
+     className={styles.hero_img}
+     />
+     </p>
+     <div className={`${styles.hero_text} sa-animation sa-fade-left `}>
         <p>
-          Hello Next.js &nbsp;
-          <code className={styles.code}><Link href="/post">Next Page</Link></code>
+          I am ZayarMoeKaung
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <h1>Full Stack Developer</h1>
+        <h1>DevOp</h1>
+        <h1>Software Engineer</h1>
+        <h1>FreeLance</h1>
+     </div>
+      
+    
+     </div>
     </main>
   )
 }
