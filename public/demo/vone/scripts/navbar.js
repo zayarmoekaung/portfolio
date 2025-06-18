6aef56405e56d26667ef7018720bfa8bbf4ea751
zayarmoekaung <zayarmoekaung0@gmail.com>
2025-06-18 10:43:50 +0630

    vone code test

---
+++ public/demo/vone/scripts/navbar.js
@@ -0,0 +1,39 @@
+function goto(elementId) {
+     const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+function resetActive(){
+const activeElements = document.getElementsByClassName('active');
+for (let i = 0; i < activeElements.length; i++) {
+    activeElements[i].classList.remove('active');
+  }
+}
+function home() {
+    window.location.href = "/demo/vone/index.html";
+}
+function about() {
+    goto("about");
+    const about_btn = document.getElementById("about_btn");
+    resetActive();
+    if (about_btn) {
+        about_btn.classList.add("active");
+    }
+}
+function contact() {
+    goto("contact");
+    const contact_btn = document.getElementById("contact_btn");
+    resetActive();
+    if (contact_btn) {
+        contact_btn.classList.add("active");
+    }
+}
+function projects() {
+    goto("projects");
+    const projects_btn = document.getElementById("projects_btn");
+    resetActive();
+    if (projects_btn) {
+        projects_btn.classList.add("active");
+    }
+}
